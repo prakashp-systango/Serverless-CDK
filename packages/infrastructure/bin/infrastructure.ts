e709da56d8928ad0f06183f78ce0c148318e2cd6
prakash-systango <ppatel@systango.com>
2024-11-29 12:05:01 +0530

    fix : clean up

--- packages/infrastructure/bin/infrastructure.ts
+++ packages/infrastructure/bin/infrastructure.ts
@@ -6,7 +6,7 @@ import { InfrastructureStack } from '../lib/infrastructure-stack';
 const app = new cdk.App();
 new InfrastructureStack(app, 'InfrastructureStack', {
   env: {
-    account: process.env.AWS_ACCESS_KEY_ID || "069502059108",
-    region: process.env.AWS_REGION || "ap-northeast-1",
+    account: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION,
   },
 });
